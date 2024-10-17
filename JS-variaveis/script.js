b4cd9b63b17cbc8ce7a1f62023f795ca7b73ac10
piRIKIT0 <134180830+piRIKIT0@users.noreply.github.com>
2024-10-17 11:20:53 -0300

    JS-variaveis
    JS-variaveis

---
+++ JS-variaveis/script.js
@@ -0,0 +1,14 @@
+
+const nome = "josé";
+const sobrenome = "marinho";
+var email = "josémarinho@gmail.com";
+var telefone = "41 994002892";
+let idade = 28;
+const sexo = "masculino";
+
+alert(nome);
+alert(sobrenome);
+alert(email);
+alert(telefone);
+alert(idade);
+alert(sexo);
