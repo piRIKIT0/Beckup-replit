dcea60727a1899968d88f077912a0c4510f86bbd
piRIKIT0 <134180830+piRIKIT0@users.noreply.github.com>
2024-10-17 11:21:54 -0300

    js mobile
    js mobile

---
+++ JS-MOBILE/if_else/ifElse.js
@@ -0,0 +1,14 @@
+const cor = prompt(" Qual sua cor favorita? ")
+
+if (cor == "vermelho") {
+  console.log("vermelho")
+}
+
+else if (cor == "azul"){
+  console.log("azul")
+}
+
+else {
+  console.log("cor invalida")
+}
+console.log(cor.length)
