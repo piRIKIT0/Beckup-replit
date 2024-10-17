97df059d8f99db7917043240ba385c4ccb8e3056
piRIKIT0 <134180830+piRIKIT0@users.noreply.github.com>
2024-10-17 11:21:23 -0300

    introduçao js
    introduçao js

---
+++ introducaojs/script.js
@@ -0,0 +1,16 @@
+//const idade = 17
+//crie uma variavel para cada um dos
+//dados listados: (nome, sobrenome, telefone, estaCasado = true ou false)
+//imprima cada um dos valores com consle.log(variavel)
+//console.log(idade)
+
+const nome = "jairzinho";
+const idade = 35;
+const sobrenome = "beira-mar";
+const telefone = 998402356;
+const estaCasado = true;
+console.log(nome);
+console.log(sobrenome);
+console.log(telefone);
+console.log(estaCasado);
+console.log(idade);
