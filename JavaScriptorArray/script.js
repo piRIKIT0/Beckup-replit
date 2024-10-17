9d1f64b9f34f2d69875cae81aad6ad64231fddc5
piRIKIT0 <134180830+piRIKIT0@users.noreply.github.com>
2024-10-17 11:23:58 -0300

    array javascript
    array javascript

---
+++ JavaScriptorArray/script.js
@@ -0,0 +1,14 @@
+//variaveis unitarias
+const nome1 = 'joão';
+const nome2 = 'maria';
+const nome3 = 'jefferson';
+
+//variaveis de array
+const nomes1 = ['joão', 'maria', 'jefferson'];
+const nomes2 = ['geraldo', 'luciana', 'julio'];
+//imprimndo valores de array
+nomes1[10] = 'Ramon Dino'
+console.log(nomes1[10]);//criando dinamicamente
+
+
+
